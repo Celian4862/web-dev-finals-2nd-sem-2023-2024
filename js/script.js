da98299785cf00e0b81765b18d8bd907c1c126f9
MrChooey <ronpatrickramas7@gmail.com>
2024-05-24 00:03:16 +0800

    added popup js (pls fix the js for me kurty)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let popup = document.getElementById("popup")
+
+function openPopup() {
+	popup.style.display = "block"
+	// popup.style.opacity = 1
+	// popup.style.transition = "opacity 0.5s"
+	// popup.style.transition = "display 0.5s"
+	// popup.style.transition = "visibility 0.5s"
+}
+
+function hidePopup() {
+	popup.style.display = "none"
+	// popup.style.opacity = 0
+	// popup.style.transition = "opacity 0.5s"
+	// popup.style.transition = "display 0.5s"
+	// popup.style.transition = "visibility 0.5s"
+}

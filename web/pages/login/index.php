c8147b07606e40d55b46a2611ed21f9ab266a23a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-06-26 00:54:33 +0800

    Update web/pages/login/index.php
    Changed: bg-building -> bg-[url(...)]
    Change: format

--- web/pages/login/index.php
+++ web/pages/login/index.php
@@ -7,63 +7,33 @@ $invalidPassword = isset($_SESSION["error"]) && isset($_SESSION["error"]["passwo
 $previousEmail = $_SESSION["previous"]["email"] ?? "";
 ?>
 
-<div class="h-dvh bg-building bg-blend-darken backdrop-brightness-75">
+<div class="h-dvh bg-[url(/assets/building.jpg)] bg-blend-darken backdrop-brightness-75">
     <div class="w-3/4 h-full mx-auto py-12 bg-primary text-white shadow-2xl">
-        <div class="
-            flex justify-center items-center w-fit mx-auto mb-4 p-6
-            aspect-square rounded-full bg-white shadow-xl
-        ">
+        <div class="flex justify-center items-center w-fit mx-auto mb-4 p-6 aspect-square rounded-full bg-white shadow-xl">
             <span class="h-fit text-8xl text-logo font-logo">N2N</span>
         </div>
         <h1 class="mb-12 text-center text-4xl font-bold drop-shadow-xl">N2N SOLUTIONS</h1>
         <form action="/login/handler" method="POST" class="w-full max-w-[400px] mx-auto">
             <div class="flex flex-col mb-8">
                 <label for="email" class="font-bold">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    required
-                    value="<?= $previousEmail ?>"
-                    data-error="<?=
-                    var_export($invalidEmail) ?>"
-                    class="
-                            p-2 rounded-md outline-none text-black ring-gray-300
-                            hover:ring focus:ring transition-colors shadow-md
-                            data-[error='true']:ring-red-400 data-[error='true']:ring
-                        "
-                />
+                <input type="email" id="email" name="email" required value="<?= $previousEmail ?>" data-error="<?= var_export($invalidEmail) ?>" class="login-input" />
                 <?php if ($invalidEmail) : ?>
                     <span class="mt-1 text-red-400 text-sm font-bold">Email not found</span>
                 <?php endif; ?>
             </div>
             <div class="flex flex-col mb-8">
                 <label for="password" class="font-bold">Password</label>
-                <input type="password" id="password" name="password" required data-error="<?= var_export(
-                    $invalidPassword
-                ) ?>" class="
-                    p-2 rounded-md outline-none text-black ring-gray-300
-                    hover:ring focus:ring transition-colors shadow-md
-                    data-[error='true']:ring-red-400 data-[error='true']:ring
-                " />
+                <input type="password" id="password" name="password" required data-error="<?= var_export($invalidPassword) ?>" class="login-input" />
                 <?php if ($invalidPassword) : ?>
                     <span class="mt-1 text-red-400 text-sm font-bold">Incorrect Password</span>
                 <?php endif; ?>
             </div>
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                    <input type="checkbox" id="remember" name="remember" class="
-                        peer relative appearance-none w-5 h-5 border rounded outline-none ring-gray-300
-                        hover:ring focus:ring after:content-[''] after:absolute after:w-full after:h-full
-                        after:left-0 after:right-0 after:bg-no-repeat after:bg-center after:bg-[length:40px]
-                        checked:after:bg-[url('/assets/check.svg')]
-                    ">
+                    <input type="checkbox" id="remember" name="remember" class="login-checkbox">
                     <label for="remember" class="ml-2">Remember me</label>
                 </div>
-                <button type="submit" class="
-                    px-4 py-2 rounded-md outline-none bg-green-500 ring-green-500 font-bold hover:bg-green-600
-                    hover:ring focus:bg-green-600 focus:ring transition-colors shadow-md
-                ">Login</button>
+                <button type="submit" class="login-submit">Login</button>
             </div>
         </form>
     </div>

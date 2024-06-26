87bb627d61a31fe6620dacf81a96814125d89090
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-06-27 05:28:35 +0800

    Update sidebar.php
    Added: active variable
    Refactor: data-active

--- web/components/views/sidebar.php
+++ web/components/views/sidebar.php
@@ -3,6 +3,14 @@
 use function Utilities\getURLPath;
 
 $path = getURLPath();
+
+$active = [
+    "dashboard" => $path === "/dashboard" || $path === "/dashboard\/",
+    "users" => preg_match("/dashboard\/users/i", $path) > 0,
+    "inventory" => preg_match("/dashboard\/inventory/i", $path) > 0,
+    "shipments" => preg_match("/dashboard\/shipments/i", $path) > 0,
+    "receptions" => preg_match("/dashboard\/receptions/i", $path) > 0,
+];
 ?>
 
 <div class="flex flex-col justify-between w-full max-w-[300px] h-dvh p-4 bg-primary text-white">
@@ -12,31 +20,31 @@ $path = getURLPath();
         </div>
         <ul class="mb-auto text-md font-bold">
             <li class="mb-2">
-                <a href="/dashboard" data-active="<?= var_export($path === "/dashboard"); ?>" class="sidebar-button">
+                <a href="/dashboard" data-active="<?= var_export($active["dashboard"]); ?>" class="sidebar-button">
                     <i class="material-symbols-rounded text-3xl">space_dashboard</i>
                     <span>Dashboard</span>
                 </a>
             </li>
             <li class="mb-2">
-                <a href="/dashboard/users" data-active="<?= var_export($path === "/dashboard/users"); ?>" class="sidebar-button">
+                <a href="/dashboard/users" data-active="<?= var_export($active["users"]); ?>" class="sidebar-button">
                     <i class="material-symbols-rounded text-3xl">person</i>
                     <span>Users</span>
                 </a>
             </li>
             <li class="mb-2">
-                <a href="/dashboard/inventory" data-active="<?= var_export($path === "/dashboard/inventory"); ?>" class="sidebar-button">
+                <a href="/dashboard/inventory" data-active="<?= var_export($active["inventory"]); ?>" class="sidebar-button">
                     <i class="material-symbols-rounded text-3xl">inventory_2</i>
                     <span>Inventory</span>
                 </a>
             </li>
             <li class="mb-2">
-                <a href="/dashboard/shipments" data-active="<?= var_export($path === "/dashboard/shipments"); ?>" class="sidebar-button">
+                <a href="/dashboard/shipments" data-active="<?= var_export($active["shipments"]); ?>" class="sidebar-button">
                     <i class="material-symbols-rounded text-3xl">local_shipping</i>
                     <span>Shipments</span>
                 </a>
             </li>
             <li class="mb-2">
-                <a href="/dashboard/receptions" data-active="<?= var_export($path === "/dashboard/receptions"); ?>" class="sidebar-button">
+                <a href="/dashboard/receptions" data-active="<?= var_export($active["receptions"]); ?>" class="sidebar-button">
                     <i class="material-symbols-rounded text-3xl">orders</i>
                     <span>Receptions</span>
                 </a>

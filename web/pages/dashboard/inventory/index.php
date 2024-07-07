2b174fbbf85e6d17677b5f859fa7f8e9c539a6b0
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:43:16 +0800

    Update web/pages/dashboard/inventory/index.php
    Changed: started using `Helper` class
    Changed: sql query

--- web/pages/dashboard/inventory/index.php
+++ web/pages/dashboard/inventory/index.php
@@ -4,12 +4,11 @@ session_start();
 
 use Components\Sidebar;
 use Components\DashboardTable;
+use Utilities\Helper;
 
-use function Utilities\getDatabase;
+$db = Helper::getDatabase();
 
-$db = getDatabase();
-
-$products = $db->query("SELECT id, label, description, array::len(->individualProduct.out) as stock FROM product");
+$products = $db->query("SELECT id, label, description, count(->stocks.in) AS stocks FROM product;");
 ?>
 
 <div class="flex">
@@ -17,9 +16,10 @@ $products = $db->query("SELECT id, label, description, array::len(->individualPr
     <div class="dashboard-container">
         <div class="dashboard-header">
             <div class="flex items-center gap-2">
-                <i class="material-symbols-rounded text-4xl">inventory_2</i>
+                <span class="material-symbols-rounded text-4xl">inventory_2</span>
                 <h1 class="text-3xl font-semibold">Inventory</h1>
             </div>
+            <a href="/dashboard/inventory/add-new" class="button-primary">Add Product</a>
         </div>
         <div class="dashboard-content">
             <?php
@@ -31,7 +31,7 @@ $products = $db->query("SELECT id, label, description, array::len(->individualPr
                         $product["id"],
                         $product["label"],
                         $product["description"],
-                        $product["stock"]
+                        $product["stocks"]
                     ];
                 },
                 headerStyle: fn ($column) => match ($column) {

231440885fba8b3ac09ad35066901d66a882744f
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:49:29 +0800

    Update Employee.php
    Changed: started using `Helper` class
    Changed: `login` function, started using `password_verify` for password authentication
    Removed: properties of `Employee` class
    Removed: `__contructor` function
    Removed: `getFullName` function
    Removed: `getContact` function

--- web/utilities/Employee.php
+++ web/utilities/Employee.php
@@ -2,15 +2,14 @@
 
 namespace Utilities;
 
-use function Utilities\getDatabase;
+use Utilities\Helper;
 
 class Employee
 {
     public readonly string $id;
     public readonly string $email;
+    public readonly array $details;
     public readonly array $time;
-
-    private array $details;
 
     public function __construct(array $employee)
     {
@@ -20,37 +19,18 @@ class Employee
         $this->time = $employee["time"];
     }
 
-    /** Returns the full name of the employee. */
-    public function getFullName(): string
-    {
-        $fullName = $this->details["fullName"];
-
-        return $fullName["firstName"] . ' ' . $fullName["middleName"][0] . '. ' . $fullName["lastName"];
-    }
-
-    public function getContact(): string
-    {
-        $contact = $this->details["contact"];
-
-        return $contact["phoneNumber"] . ' | ' . $contact["email"];
-    }
-
     /** Creates a new employee object if the credentails are correct. */
     public static function login(string $email, string $password): self|string
     {
-        $db = getDatabase();
-
-        $employee = $db->query("SELECT * FROM ONLY employee WHERE email = '$email' LIMIT 1");
+        $employee = Helper::getDatabase()->query("SELECT * FROM ONLY employee WHERE email = '$email' LIMIT 1");
 
         if ($employee == null) {
             return "email";
         }
 
-        if ($employee["password"] !== $password) {
+        if (password_verify($password, $employee["password"]) === false) {
             return "password";
         }
-
-        $db->disconnect();
 
         return new Employee($employee);
     }

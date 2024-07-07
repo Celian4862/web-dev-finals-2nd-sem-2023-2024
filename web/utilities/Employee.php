f339571c6589a536464e9358b01bbc151cab8475
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:49:58 +0800

    Update Employee.php
    Removed: properties

--- web/utilities/Employee.php
+++ web/utilities/Employee.php
@@ -6,21 +6,8 @@ use Utilities\Helper;
 
 class Employee
 {
-    public readonly string $id;
-    public readonly string $email;
-    public readonly array $details;
-    public readonly array $time;
-
-    public function __construct(array $employee)
-    {
-        $this->id = $employee["id"];
-        $this->email = $employee["email"];
-        $this->details = $employee["details"];
-        $this->time = $employee["time"];
-    }
-
     /** Creates a new employee object if the credentails are correct. */
-    public static function login(string $email, string $password): self|string
+    public static function login(string $email, string $password): string
     {
         $employee = Helper::getDatabase()->query("SELECT * FROM ONLY employee WHERE email = '$email' LIMIT 1");
 
@@ -32,6 +19,6 @@ class Employee
             return "password";
         }
 
-        return new Employee($employee);
+        return "JWT";
     }
 }

c49b3f1681744ea18f8d15a87700568e123396ff
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:51:02 +0800

    Update web/pages/logout/index.php
    Changed: started using `Helper` class

--- web/pages/logout/index.php
+++ web/pages/logout/index.php
@@ -1,11 +1,11 @@
 <?php
 
-use function Utilities\redirect;
+use Utilities\Helper;
 
 if ($_SERVER["REQUEST_METHOD"] === "DELETE" || isset($_GET["_method"]) && $_GET["_method"] === "DELETE") {
     session_start();
     unset($_SESSION["employee"]);
-    redirect("/login");
+    Helper::redirect("/login");
 } else {
-    redirect("/404");
+    Helper::redirect("/404");
 }

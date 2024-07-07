71122a0063df2becb6d019b3ab2dbd1ca6fd70cb
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:21:11 +0800

    Update tailwind.config.js
    Added: css to content
    Changed: textColor from logo to highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./web/pages/**/*.php", "./web/**/views/**/*.php"],
+	content: ["./web/pages/**/*.{php, css}", "./web/**/views/**/*.{php,css}"],
 	theme: {
 		extend: {
 			backgroundColor: {
 				primary: "#406191",
 			},
 			textColor: {
-				logo: "#406191",
+				highlight: "#406191",
 			},
 			fontFamily: {
 				logo: ["Protest Guerrilla"],

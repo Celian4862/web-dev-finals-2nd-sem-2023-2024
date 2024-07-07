7c52fe6c733bb434b911544d68107336229c60e2
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-08 01:30:10 +0800

    Update sidebar.php
    Added: clients to navigations
    Added: distributor to navigations
    Changed: started using Helper class
    Changed: class `text-logo` was changed to `text-highlight`
    Changed: started using `span` instead of `i` tag

--- web/components/views/sidebar.php
+++ web/components/views/sidebar.php
@@ -1,8 +1,8 @@
 <?php
 
-use function Utilities\getURLPath;
+use Utilities\Helper;
 
-$path = getURLPath();
+$path = Helper::getURLPath();
 
 $navigations = [
     [
@@ -16,6 +16,18 @@ $navigations = [
         "icon" => "person",
         "link" => "/dashboard/employees",
         "active" => preg_match("/dashboard\/employees/i", $path) > 0,
+    ],
+    [
+        "title" => "Clients",
+        "icon" => "group",
+        "link" => "/dashboard/clients",
+        "active" => preg_match("/dashboard\/clients/i", $path) > 0,
+    ],
+    [
+        "title" => "Distributor",
+        "icon" => "warehouse",
+        "link" => "/dashboard/distributors",
+        "active" => preg_match("/dashboard\/distributors/i", $path) > 0,
     ],
     [
         "title" => "Inventory",
@@ -34,20 +46,20 @@ $navigations = [
         "icon" => "orders",
         "link" => "/dashboard/receptions",
         "active" => preg_match("/dashboard\/receptions/i", $path) > 0,
-    ]
+    ],
 ];
 ?>
 
 <div class="flex flex-col justify-between w-full max-w-[300px] h-dvh p-4 bg-primary text-white">
     <div>
         <div class="flex justify-center items-center w-fit mx-auto mb-8 p-6 aspect-square rounded-full bg-white shadow-xl">
-            <span class="h-fit text-8xl text-logo font-logo">N2N</span>
+            <span class="h-fit select-none text-8xl text-highlight font-logo">N2N</span>
         </div>
         <ul class="mb-auto text-md font-bold">
             <?php foreach ($navigations as $navigation) : ?>
                 <li class="mb-2">
                     <a href="<?= $navigation["link"]; ?>" data-active="<?= var_export($navigation["active"]); ?>" class="sidebar-button">
-                        <i class="material-symbols-rounded text-3xl"><?= $navigation["icon"]; ?></i>
+                        <span class="material-symbols-rounded text-3xl"><?= $navigation["icon"]; ?></span>
                         <span><?= $navigation["title"]; ?></span>
                     </a>
                 </li>
@@ -57,7 +69,7 @@ $navigations = [
     <form action="/logout" method="DELETE" class="font-bold">
         <button type="submit" name="_method" value="DELETE" class="sidebar-button float-end">
             <span>Logout</span>
-            <i class="material-symbols-rounded text-3xl">logout</i>
+            <span class="material-symbols-rounded text-3xl">logout</span>
         </button>
     </form>
 </div>

d751898893237c6b6cfa8de0a5ce4b4a82554660
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-13 11:41:41 +0800

    Update web/pages/dashboard/index.php
    Added: `Orders` to `cards`
    Changed: queries of `cards`
    Removed: `Client` from `cards`
    Removed: `Distributor` from `cards`

--- web/pages/dashboard/index.php
+++ web/pages/dashboard/index.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start();
+
 
 use Components\Sidebar;
 use Components\DashboardTable;
@@ -8,54 +8,38 @@ use Utilities\Helper;
 
 $db = Helper::getDatabase();
 
-$products = $db->query(<<<SQL
-    SELECT
-        id,
-        label,
-        description,
-        count(->stocks.in) AS stocks,
-        time.createdAt as createdAt
-    FROM product
-    ORDER BY createdAt DESC
-    LIMIT 10;
-SQL);
+$query = Helper::getURLQuery();
 
 $cards = [
     [
         "title" => "Employees",
         "icon" => "person",
         "link" => "/employees",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM employee where time.deletedAt = none) }")["count"],
+        "description" => $db->query("array::len(SELECT id FROM employee where time.deletedAt IS NONE);"),
     ],
     [
-        "title" => "Client",
+        "title" => "Persons",
         "icon" => "group",
-        "link" => "/client",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM client) }")["count"],
-    ],
-    [
-        "title" => "Distributor",
-        "icon" => "warehouse",
-        "link" => "/distributors",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM distributor) }")["count"],
+        "link" => "/persons",
+        "description" => $db->query("array::len(SELECT id FROM person);"),
     ],
     [
         "title" => "Inventory",
         "icon" => "inventory_2",
         "link" => "/inventory",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM physicalProduct) }")["count"],
+        "description" => $db->query("array::len(SELECT id FROM physicalProduct);"),
     ],
     [
-        "title" => "Shipments",
-        "icon" => "local_shipping",
-        "link" => "/shipments",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM shipment) }")["count"],
+        "title" => "Orders",
+        "icon" => "list_alt",
+        "link" => "/orders",
+        "description" => $db->query("array::len(SELECT id FROM order);"),
     ],
     [
         "title" => "Receptions",
         "icon" => "orders",
         "link" => "/receptions",
-        "description" => $db->query("RETURN { count: array::len(SELECT id FROM reception) }")["count"],
+        "description" => $db->query("array::len(SELECT id FROM reception);"),
     ]
 ]
 ?>
@@ -70,10 +54,10 @@ $cards = [
                 <h1 class="text-3xl font-semibold">Dashboard</h1>
             </div>
         </div>
-        <div class="dashboard-content flex flex-col gap-8">
+        <div class="dashboard-content flex flex-col gap-4">
             <div class="flex justify-between gap-8">
                 <?php foreach ($cards as $card) : ?>
-                    <a href="/dashboard<?= $card["link"]; ?>" class="relative flex flex-col w-full p-4 rounded-lg bg-[#0086B2] ring-[#0086B280] text-white shadow-xl hover:bg-[#037CA3] hover:ring focus:bg-[#037CA3] focus:ring transition-colors">
+                    <a href="/dashboard<?= $card["link"]; ?>" class="relative flex flex-col w-full p-4 rounded-lg bg-[#0086B2] ring-[#0086B280] text-white shadow-lg hover:bg-[#037CA3] hover:ring focus:bg-[#037CA3] focus:ring transition-colors">
                         <span class="material-symbols-rounded absolute right-0 bottom-0 mr-2 text-6xl text-[#0C6787]"><?= $card["icon"]; ?></span>
                         <span class="text-2xl font-bold z-10"><?= $card["description"]; ?></span>
                         <span class="mb-6 z-10"><?= $card["title"]; ?></span>
@@ -87,25 +71,66 @@ $cards = [
                 </div>
             </div>
             <?php
+            $receptions = $db->query(<<<SQL
+            SELECT * FROM (
+                SELECT
+                    id,
+                    distributor.person as distributor.id,
+                    distributor.person.name AS distributor.name,
+                    delivery.dateShipped AS dateShipped,
+                    time.createdAt AS createdAt,
+                    (
+                        SELECT
+                            out as id,
+                            out.name AS name,
+                            time.createdAt
+                        FROM ONLY delivery->deliveryStatusLine
+                        WHERE time.deletedAt IS NONE
+                        ORDER BY time.createdAt DESC
+                        LIMIT 1
+                    ) AS status
+                FROM reception
+                WHERE time.deletedAt IS NONE
+                LIMIT 10
+            ) WHERE status.id = deliveryStatus:2;
+            SQL);
+
             DashboardTable::render(
-                $products,
-                ["Product ID", "Label", "Description", "Stock"],
-                elementData: function ($product) {
+                $receptions,
+                ["Tracking ID", "Distributor", "Date Shipped", "Status"],
+                function ($reception) use ($query) {
+                    $id = $reception["id"];
+                    $infoQuery = Helper::getURLPathQuery("dashboard/receptions", array_merge($query, ["info" => $id]));
+                    $distributorQuery = Helper::getURLPathQuery("/dashboard/persons", array_merge($query, ["info" => $reception["distributor"]["id"]]));
+                    $status = match ($reception["status"]["name"] ?? "") {
+                        "" => "",
+                        "Delivered" => <<<HTML
+                        <span class="px-2 py-1 rounded-full bg-green-500 text-white font-semibold">{$reception["status"]["name"]}</span>
+                        HTML,
+                        default => <<<HTML
+                        <span class="px-2 py-1 rounded-full bg-gray-400 text-white font-semibold">{$reception["status"]["name"]}</span>
+                        HTML
+                    };
+
                     return [
-                        $product["id"],
-                        $product["label"],
-                        $product["description"],
-                        $product["stocks"]
+                        <<<HTML
+                        <a href="{$infoQuery}" class="dashboard-table-id">$id</a>
+                        HTML,
+                        <<<HTML
+                        <a href="{$distributorQuery}" class="dashboard-table-id">{$reception["distributor"]["name"]}</a>
+                        HTML,
+                        date_create($reception["dateShipped"])->format("M, d, Y"),
+                        $status,
                     ];
                 },
-                search: false,
+                allowSearch: fn () => false,
                 headerStyle: fn ($column) => match ($column) {
-                    "Stock" => "text-align: center;",
+                    "Date Shipped", "Status" => "align-items: center",
                     default => ""
                 },
                 rowStyle: fn ($column) => match ($column) {
-                    "Stock" => "text-align: center;",
-                    default => ""
+                    "Date Shipped", "Status" => "text-align: center;",
+                    default => "",
                 }
             );
             ?>
@@ -116,24 +141,51 @@ $cards = [
                 </div>
             </div>
             <?php
+            $products = $db->query(<<<SQL
+            SELECT
+                id,
+                label,
+                description,
+                desiredStocks,
+                array::len((
+                    SELECT
+                        id,
+                        status.name AS status
+                    FROM ->stock
+                    WHERE
+                        time.deletedAt IS NONE AND
+                        status = stockStatus:0
+                )) AS physicalStocks,
+                time.createdAt AS createdAt
+            FROM product
+            WHERE time.deletedAt IS NONE
+            LIMIT 10;
+            SQL);
+
             DashboardTable::render(
                 $products,
-                ["Product ID", "Label", "Description", "Stock"],
-                elementData: function ($product) {
+                ["Product ID", "Label", "Description", "Desired Stocks", "Physical Stocks"],
+                function ($product) {
+                    $id = $product["id"];
+                    $infoQuery = Helper::getURLPathQuery("/dashboard/inventory", query: ["info" => $id]);
+
                     return [
-                        $product["id"],
+                        <<<HTML
+                        <a href="{$infoQuery}" class="dashboard-table-id">$id</a>
+                        HTML,
                         $product["label"],
                         $product["description"],
-                        $product["stocks"]
+                        $product["desiredStocks"],
+                        $product["physicalStocks"]
                     ];
                 },
-                search: false,
+                allowSearch: fn () => false,
                 headerStyle: fn ($column) => match ($column) {
-                    "Stock" => "text-align: center;",
+                    "Physical Stocks", "Desired Stocks" => "align-items: center;",
                     default => ""
                 },
                 rowStyle: fn ($column) => match ($column) {
-                    "Stock" => "text-align: center;",
+                    "Physical Stocks", "Desired Stocks" => "text-align: center;",
                     default => ""
                 }
             );

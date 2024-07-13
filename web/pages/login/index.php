a693f2992c741ae34b6225f8cb05fa6c0a2ba19b
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-13 11:48:33 +0800

    Update web/pages/login/index.php

--- web/pages/login/index.php
+++ web/pages/login/index.php
@@ -1,7 +1,13 @@
 <?php
 
-session_start();
+use Utilities\Auth;
+use Utilities\Helper;
 
+if (Auth::authenticate($_COOKIE["token"] ?? $_SESSION["token"] ?? "")) {
+    Helper::redirect("/dashboard");
+}
+
+$notAdmin = isset($_SESSION["error"]) && isset($_SESSION["error"]["notAdmin"]);
 $invalidEmail = isset($_SESSION["error"]) && isset($_SESSION["error"]["email"]);
 $invalidPassword = isset($_SESSION["error"]) && isset($_SESSION["error"]["password"]);
 $previousEmail = $_SESSION["previous"]["email"] ?? "";
@@ -16,7 +22,10 @@ $previousEmail = $_SESSION["previous"]["email"] ?? "";
         <form action="/login/handler" method="POST" class="w-full max-w-[400px] mx-auto">
             <div class="input-box mb-8">
                 <label for="email" class="font-bold">Email</label>
-                <input type="email" id="email" name="email" required value="<?= $previousEmail ?>" data-error="<?= var_export($invalidEmail) ?>" />
+                <input type="email" id="email" name="email" required value="<?= $previousEmail ?>" data-error="<?= var_export($invalidEmail || $notAdmin) ?>" />
+                <?php if ($notAdmin) : ?>
+                    <span class="text-red-400 text-sm font-bold">Not an admin email</span>
+                <?php endif; ?>
                 <?php if ($invalidEmail) : ?>
                     <span class="text-red-400 text-sm font-bold">Email not found</span>
                 <?php endif; ?>
@@ -30,7 +39,7 @@ $previousEmail = $_SESSION["previous"]["email"] ?? "";
             </div>
             <div class="flex items-center justify-between">
                 <label for="remember" class="flex items-center gap-2">
-                    <div class="login-checkbox">
+                    <div class="input-checkbox">
                         <input type="checkbox" id="remember" name="remember">
                         <span class="material-symbols-rounded">check</span>
                     </div>
@@ -43,6 +52,5 @@ $previousEmail = $_SESSION["previous"]["email"] ?? "";
 </div>
 
 <?php
-unset($_SESSION["error"]);
-unset($_SESSION["previous"]);
+unset($_SESSION["error"], $_SESSION["previous"]);
 ?>

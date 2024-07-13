3c864e4f089dd9333e814cdccb9f93f870acfc24
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-13 11:48:45 +0800

    Update web/pages/logout/index.php

--- web/pages/logout/index.php
+++ web/pages/logout/index.php
@@ -3,8 +3,9 @@
 use Utilities\Helper;
 
 if ($_SERVER["REQUEST_METHOD"] === "DELETE" || isset($_GET["_method"]) && $_GET["_method"] === "DELETE") {
-    session_start();
-    unset($_SESSION["employee"]);
+    session_unset();
+    unset($_COOKIE["token"]);
+    setcookie("token", "", -1, "/");
     Helper::redirect("/login");
 } else {
     Helper::redirect("/404");
